d48a5b32c72e543b2c77e97c5cb1094d10452b1e
Hoai N. Vo <hoangvohoaingoc@gmail.com>
2024-04-02 17:33:36 -0400

    control2

--- control/script.js
+++ control/script.js
@@ -3,6 +3,7 @@ $(".btn1").click(function(){
     $(this).hide();
     $(".btn2").addClass("btn2after");
     $(".btn3").addClass("btn3after");
+    $(".char").addClass("charafter");
 });
 
 $(".btn2").click(function(){
@@ -10,24 +11,43 @@ $(".btn2").click(function(){
     $(this).hide();
     $(".btn1").addClass("btn1after");
     $(".btn3").addClass("btn3after");
+    $(".char").addClass("charafter");
 });
 
 $(".btn3").click(function(){
     $(".popup").addClass("open-popup");
+    $(this).hide();
     $(".btn1").addClass("btn1after");
     $(".btn2").addClass("btn2after");
+    $(".char").addClass("charafter");
 });
 
 $(".popup button").click(function(){
     $(".popup").removeClass("open-popup");
     $(".basket").addClass("chosenbasket");
     $(".popup1").addClass("open-popup1");
+    $(".farm").addClass("farmafter");
+    $(".berry").addClass("berryafter");
+    $(".berry1").addClass("berryafter");
+    $(".berry2").addClass("berryafter");
+    $(".berry3").addClass("berryafter");
+    $(".berry4").addClass("berryafter");
+    $(".berry5").addClass("berryafter");
+    $(".berry6").addClass("berryafter");
+    $(".berry7").addClass("berryafter");
+    $(".berry8").addClass("berryafter");
+    $(".berry9").addClass("berryafter");
  });
 
  $(".popup1 button").click(function(){
     $(".popup1").removeClass("open-popup1");
- });
- 
+    $(".farm").removeClassClass("farmafter");
+    $(".berry").removeClass("berryafter");
+    $(".berry1").removeClass("berryafter");
+    $(".popup2").addClass("open-popup2");
+}
+);
+
  $( function() {
-    $( ".draggable" ).draggable({ snap: true });
+    $( ".draggable" ).draggable();
  } );
